0e516da1e50b46f95fb052b85a11cf90dae54762
Kien637 <regviothanlong@gmail.com>
2026-05-14 14:03:34 +0700

    Add project files.

---
+++ CMS.Data/Entities/Category.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Sinh Vien: Vo Trung Kien
+ * Ma SV: 2123110044
+ * Ngay Tao: 14/5/2026
+ * Version 1.0
+ **/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.Data.Entities
+{
+
+    public class Category
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; } // Tên danh mục (vd: Tin Giáo Dục) 
+        public string Description { get; set; }
+
+        // Quan hệ: Một danh mục có nhiều bài viết 
+
+        public virtual ICollection<Post> Posts { get; set; }
+
+    }
+
+}
---
+++ CMS.Data/Entities/User.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Sinh Vien: Vo Trung Kien
+ * Ma SV: 2123110044
+ * Ngay Tao: 14/5/2026
+ * Version 1.0
+ **/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.Data.Entities
+{
+
+    public class User
+    {
+
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string PasswordHash { get; set; }
+
+        public string FullName { get; set; }
+
+        public string Role { get; set; } // Quản trị viên hoặc Biên tập viên 
+
+    }
+
+}
