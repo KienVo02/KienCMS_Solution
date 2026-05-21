4ba2f0d431daba980f7ddf0270904101feb6bb8d
KienVo02 <accvippro100@gmail.com>
2026-05-21 16:08:38 +0700

    Hoàn Thành Buổi 3.

--- CMS.Backend/Controllers/HomeController.cs
+++ CMS.Backend/Controllers/HomeController.cs
@@ -1,21 +1,33 @@
 using CMS.Backend.Models;
+using CMS.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using System.Linq;
 
 namespace CMS.Backend.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        // Constructor Injection
+        public HomeController(ApplicationDbContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
+        // Trang ch?
         public IActionResult Index()
         {
-            return View();
+            // L?y 3 bài vi?t m?i nh?t
+            var latestPosts = _context.Posts
+                                      .Include(p => p.Category)
+                                      .OrderByDescending(p => p.CreatedDate)
+                                      .Take(3)
+                                      .ToList();
+
+            return View(latestPosts);
         }
 
         public IActionResult Privacy()
@@ -23,10 +35,17 @@ namespace CMS.Backend.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        [ResponseCache(Duration = 0,
+                       Location = ResponseCacheLocation.None,
+                       NoStore = true)]
+
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new ErrorViewModel
+            {
+                RequestId = Activity.Current?.Id
+                            ?? HttpContext.TraceIdentifier
+            });
         }
     }
 }
--- CMS.Backend/Controllers/PostController.cs
+++ CMS.Backend/Controllers/PostController.cs
@@ -1,5 +1,7 @@
 ﻿using CMS.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace CMS.Backend.Controllers
 {
@@ -13,12 +15,49 @@ namespace CMS.Backend.Controllers
             _context = context;
         }
 
-        // Action Index
+        // Danh sách bài viết
         public IActionResult Index()
         {
-            var posts = _context.Posts.ToList();
+            var posts = _context.Posts
+                                .Include(p => p.Category)
+                                .OrderByDescending(p => p.CreatedDate)
+                                .ToList();
 
             return View(posts);
+        }
+
+        public IActionResult Details(int id)
+
+        {
+
+            // 1. Truy vấn bài viết theo ID 
+
+            // Sử dụng .Include(p => p.Category) để lấy kèm thông tin Danh mục (Join bảng) 
+
+            var post = _context.Posts
+
+                .Include(p => p.Category)
+
+                .FirstOrDefault(p => p.Id == id);
+
+
+
+            // 2. Kiểm tra nếu không tìm thấy bài viết (tránh lỗi màn hình trắng) 
+
+            if (post == null)
+
+            {
+
+                return NotFound(); // Trả về trang lỗi 404 
+
+            }
+
+
+
+            // 3. Truyền dữ liệu sang View 
+
+            return View(post);
+
         }
     }
 }
